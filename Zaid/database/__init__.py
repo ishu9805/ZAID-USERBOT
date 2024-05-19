fd9df37cbc29489f6fab83b496b3dbe9a0450fdd
ishu9805 <abhishek47903@gmail.com>
2024-05-19 17:06:59 +0530

    Update __init__.py

--- Zaid/database/__init__.py
+++ Zaid/database/__init__.py
@@ -1,6 +1,6 @@
-import motor.motor_asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from config import MONGO_URL
-cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+cli = AsyncIOMotorClient(MONGO_URL)
 
 dbb = cli.program
